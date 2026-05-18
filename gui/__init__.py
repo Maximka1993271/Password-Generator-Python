7c83e0fd1258b8d34e7c6f9ee435bb2b0bf4a892
Maximka1993271 <melnikovmaksim540@gmail.com>
2026-05-18 14:19:30 +0300

    Delete gui directory

--- gui/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-GUI module - main window and dialogs
-"""
-from gui.main_window import SecurePassPro
-from gui.dialogs import CTkMessageBox, CTkInputDialog
-from gui.widgets import ToolTip
-
-__all__ = ['SecurePassPro', 'CTkMessageBox', 'CTkInputDialog', 'ToolTip']
